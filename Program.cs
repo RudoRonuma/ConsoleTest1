23973e8ae3c91beab6e59cbac879f52efd983442
Aliwoto <woto@kaizoku.cyou>
2022-12-19 22:54:39 +0330

    Do some code refactor, disable implicit usings since it's not available in .net 5.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿namespace ConsoleTest1
+﻿using System;
+
+namespace ConsoleTest1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World! From Version 1.1.09");
         }
     }
 }
